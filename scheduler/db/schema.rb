589908b79cdee50b75aa631c29e01fec89732cdc
morganhopelee <morganhopelee@gmail.com>
2016-04-14 20:03:20 -0700

    forgot to rake lol

--- scheduler/db/schema.rb
+++ scheduler/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408025533) do
+ActiveRecord::Schema.define(version: 20160415023242) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20160408025533) do
     t.string   "instructor"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.string   "name"
+    t.string   "student"
+    t.integer  "classes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "students", force: :cascade do |t|
