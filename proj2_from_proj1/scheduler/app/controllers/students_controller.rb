b5bb18921131c7046907717bc2f7cfc9becc1906
morganhopelee <morganhopelee@gmail.com>
2016-04-20 20:34:48 -0700

    added home page

--- proj2_from_proj1/scheduler/app/controllers/students_controller.rb
+++ proj2_from_proj1/scheduler/app/controllers/students_controller.rb
@@ -1,4 +1,4 @@
 class StudentsController < ApplicationController
-  # before_filter :authenticate_student!
+  before_filter :authenticate_student!
   # DO WE NEED THIS???
 end
--- proj2_from_scratch/scheduler/config/routes.rb
+++ proj2_from_scratch/scheduler/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :students
   devise_for :admins
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
