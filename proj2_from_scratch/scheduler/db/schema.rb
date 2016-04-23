d5d2a4eeddebf938bad6732d42a95bcafbe7ef06
Scarlett Wu <swu0207@berkeley.edu>
2016-04-23 00:01:21 -0700

    added navbar with login and signup

--- proj2_from_scratch/scheduler/db/schema.rb
+++ proj2_from_scratch/scheduler/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422224217) do
+ActiveRecord::Schema.define(version: 20160423063215) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160422224217) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.boolean  "admin"
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
@@ -74,6 +75,7 @@ ActiveRecord::Schema.define(version: 20160422224217) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.boolean  "admin"
   end
 
   add_index "students", ["email"], name: "index_students_on_email", unique: true
