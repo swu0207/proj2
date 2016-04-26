ac65a2c30a88643472cd01772db9814416cf93df
morganhopelee <morganhopelee@gmail.com>
2016-04-25 17:00:03 -0700

    fixed home button

--- proj2_from_scratch/scheduler/app/controllers/home_controller.rb
+++ proj2_from_scratch/scheduler/app/controllers/home_controller.rb
@@ -3,11 +3,5 @@ class HomeController < ApplicationController
 		@students = Student.all
 		@admins = Admin.all
 		@courses = Course.all
-
-		if student_signed_in?
-			redirect_to student_show_path(id: current_student.id)
-		elsif admin_signed_in?
-			redirect_to admin_show_path(id: current_admin.id)
-		end
 	end
 end
